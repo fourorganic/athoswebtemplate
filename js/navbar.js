c10568785e2269194f143710265e1a3c7998d2d4
Neyla Johanna Rojas Pena <neylarojas.developer@gmail.com>
2026-08-10 18:29:11 +0200

    - create product page for Manteca de cerdo achiotada

--- js/navbar.js
+++ js/navbar.js
@@ -7,14 +7,17 @@
     { slug: 'protector-solar-tonka', label: 'Protector Solar de Tonka' },
     { slug: 'champu-moringa', label: 'Champú de Moringa' },
     { slug: 'jabon-achiote', label: 'Jabón de Achiote' },
-    { slug: 'mascarilla-aguacate', label: 'Mascarilla de Aguacate' }
+    { slug: 'mascarilla-aguacate', label: 'Mascarilla de Aguacate' },
+    { slug: 'manteca-cerdo-achiotada', label: 'Manteca de Cerdo Achiotada' },
   ];
 
   var path = window.location.pathname;
   var isProductPage = /\/productos\//.test(path);
   var isProductosList = !isProductPage && /\/productos\.html$/.test(path);
   var isIndex = !isProductPage && !isProductosList;
-  var currentSlug = isProductPage ? path.replace(/^.*\//, '').replace(/\.html$/, '') : null;
+  var currentSlug = isProductPage
+    ? path.replace(/^.*\//, '').replace(/\.html$/, '')
+    : null;
 
   var prefix = isProductPage ? '../' : '';
   var home = isIndex ? '#top' : prefix + 'index.html#top';
@@ -30,28 +33,50 @@
 
   var items = PRODUCTS.map(function (p) {
     var active = currentSlug === p.slug ? ' active' : '';
-    return '<li><a class="dropdown-item' + active + '" href="' + productHref(p.slug) + '">' + p.label + '</a></li>';
+    return (
+      '<li><a class="dropdown-item' +
+      active +
+      '" href="' +
+      productHref(p.slug) +
+      '">' +
+      p.label +
+      '</a></li>'
+    );
   }).join('');
 
   var html =
     '' +
     '<div class="container container-narrow py-2">' +
-    '<a class="navbar-brand" href="' + home + '"><img src="' + logo + '" alt="Fourmidable" class="navbar-logo"></a>' +
+    '<a class="navbar-brand" href="' +
+    home +
+    '"><img src="' +
+    logo +
+    '" alt="Fourmidable" class="navbar-logo"></a>' +
     '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">' +
     '<span class="navbar-toggler-icon"></span>' +
     '</button>' +
     '<div id="nav" class="collapse navbar-collapse">' +
     '<ul class="navbar-nav ms-auto align-items-lg-center gap-lg-2">' +
     '<li class="nav-item dropdown">' +
-    '<a class="nav-link dropdown-toggle' + dropdownActive + '" href="' + productos + '" id="productosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Productos</a>' +
+    '<a class="nav-link dropdown-toggle' +
+    dropdownActive +
+    '" href="' +
+    productos +
+    '" id="productosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Productos</a>' +
     '<ul class="dropdown-menu dropdown-menu-end shadow-sm" aria-labelledby="productosDropdown">' +
     items +
     '<li><hr class="dropdown-divider"></li>' +
-    '<li><a class="dropdown-item fw-semibold' + verTodosActive + '" href="' + productos + '">Ver todos los productos</a></li>' +
+    '<li><a class="dropdown-item fw-semibold' +
+    verTodosActive +
+    '" href="' +
+    productos +
+    '">Ver todos los productos</a></li>' +
     '</ul>' +
     '</li>' +
     '<li class="nav-item"><a class="nav-link" href="https://www.arckia.com" target="_blank" rel="noopener">Nutracéuticos</a></li>' +
-    '<li class="nav-item"><a class="nav-link" href="' + contacto + '">Contacto</a></li>' +
+    '<li class="nav-item"><a class="nav-link" href="' +
+    contacto +
+    '">Contacto</a></li>' +
     '<li class="nav-item ms-lg-2">' +
     '<a class="btn btn-accent btn-sm px-3" href="https://wa.me/573158927862" target="_blank" rel="noopener"><i class="bi bi-whatsapp me-1"></i>Escríbenos</a>' +
     '</li>' +
